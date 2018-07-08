797b47c839051eaf4445688b8886db4bf9232adf
Kerthin <belichenko.roden@list.ru>
2018-07-08 16:53:59 +0300

    Add main.js to combine scripts into 1 file

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+/********************
+	jquery.spincrement.min.js
+********************/
+//= partials/jquery.spincrement.min.js
+
+/********************
+	bundle.js
+********************/
+//= partials/bundle.js
+
+/********************
+	numcountSetting.js
+********************/
+//= partials/numcountSetting.js
+
+/********************
+	numcount.js
+********************/
+//= partials/numcount.js
